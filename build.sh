2ee5cafc001f8393469f3f41f198333e0cf01fe5
Mehmet Can Karagöz <78308169+mckaragoz@users.noreply.github.com>
2025-01-19 16:42:15 +0300

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 curl -sSL https://dot.net/v1/dotnet-install.sh > dotnet-install.sh 
 chmod +x dotnet-install.sh 
-./dotnet-install.sh -c 8.0 --version 8.0.100 -InstallDir ./dotnet8 
-./dotnet8/dotnet --version 
-./dotnet8/dotnet tool install Excubo.WebCompiler --global
-./dotnet8/dotnet publish CodeBeam.MudBlazor.Extensions.Docs.Wasm/CodeBeam.MudBlazor.Extensions.Docs.Wasm.csproj -c Release -o output
+./dotnet-install.sh -c 9.0 --version 9.0.100 -InstallDir ./dotnet9 
+./dotnet9/dotnet --version 
+./dotnet9/dotnet tool install Excubo.WebCompiler --global
+./dotnet9/dotnet publish CodeBeam.MudBlazor.Extensions.Docs.Wasm/CodeBeam.MudBlazor.Extensions.Docs.Wasm.csproj -c Release -o output
